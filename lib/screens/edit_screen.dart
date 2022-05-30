17e2798b857ad7412ad57dfffcb1c3dc339049dd
Hami0095 <abdman0095@gmail.com>
2022-05-30 15:48:02 +0500

    UPDATED-LOG=>TextFormFields added

--- lib/screens/edit_screen.dart
+++ lib/screens/edit_screen.dart
@@ -10,6 +10,34 @@ class EditScreen extends StatefulWidget {
 }
 
 class _EditScreenState extends State<EditScreen> {
+  final _priceFocus = FocusNode();
+  final _imgController = TextEditingController();
+  final _imageUrlFocusNode = FocusNode();
+  final _descFocusNode = FocusNode();
+  final _form = GlobalKey<FormState>();
+  @override
+  void initState() {
+    _imageUrlFocusNode.addListener(updateImgUrl);
+    super.initState();
+  }
+
+  @override
+  void dispose() {
+    _priceFocus.dispose();
+    _descFocusNode.dispose();
+    _imgController.dispose();
+    _imageUrlFocusNode.removeListener(updateImgUrl);
+    _imageUrlFocusNode.dispose();
+    super.dispose();
+  }
+
+  void updateImgUrl() {
+    if (!_imageUrlFocusNode.hasFocus) {
+      setState(() {});
+    }
+  }
+
+  void saveForm() {}
   @override
   Widget build(BuildContext context) {
     return Scaffold(
@@ -17,17 +45,81 @@ class _EditScreenState extends State<EditScreen> {
         title: const Text(
           'Edit Your Products',
         ),
+        actions: [
+          IconButton(
+              onPressed: (() => saveForm()), icon: const Icon(Icons.save))
+        ],
       ),
-      body: Form(
-        child: ListView(
-          children: [
-            TextFormField(
-              decoration: const InputDecoration(
-                label: Text('Title'),
+      body: Padding(
+        padding: const EdgeInsets.all(8.0),
+        child: Form(
+          key: _form,
+          child: ListView(
+            children: [
+              TextFor mField(
+                decoration: const InputDecoration(
+                  label: Text('Title'),
+                ),
+                textInputAction: TextInputAction.next,
+                onFieldSubmitted: (_) {
+                  FocusScope.of(context).requestFocus(_priceFocus);
+                },
               ),
-              textInputAction: TextInputAction.next,
-            )
-          ],
+              TextFormField(
+                decoration: const InputDecoration(
+                  label: Text('Price'),
+                ),
+                textInputAction: TextInputAction.next,
+                focusNode: _priceFocus,
+                keyboardType: TextInputType.number,
+              ),
+              TextFormField(
+                decoration: const InputDecoration(
+                  label: Text('Description'),
+                ),
+                maxLines: 3,
+                keyboardType: TextInputType.multiline,
+                focusNode: _descFocusNode,
+              ),
+              Row(
+                crossAxisAlignment: CrossAxisAlignment.end,
+                children: [
+                  Container(
+                    width: 100,
+                    height: 100,
+                    margin: const EdgeInsets.only(top: 8, right: 8),
+                    decoration: BoxDecoration(
+                      border: Border.all(width: 1, color: Colors.grey),
+                    ),
+                    child: Container(
+                      child: _imgController.text.isEmpty
+                          ? const Text('Enter URL')
+                          : FittedBox(
+                              child: Image.network(_imgController.text),
+                              fit: BoxFit.contain,
+                            ),
+                    ),
+                  ),
+                  Container(
+                    width: 250,
+                    child: TextFormField(
+                      decoration: const InputDecoration(labelText: 'Image Url'),
+                      keyboardType: TextInputType.url,
+                      textInputAction: TextInputAction.done,
+                      controller: _imgController,
+                      focusNode: _imageUrlFocusNode,
+                      onEditingComplete: () {
+                        setState(() {});
+                      },
+                      onFieldSubmitted: (_) {
+                        saveForm();
+                      },
+                    ),
+                  ),
+                ],
+              ),
+            ],
+          ),
         ),
       ),
     );
